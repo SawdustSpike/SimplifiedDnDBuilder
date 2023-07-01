440f6308067735ed74a9554141b68343316a151e
Michael Cowell <michaelcodesc.gmail.com>
2023-06-30 23:20:14 -0400

    Start of Character Builder Successful

---
+++ Controllers/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Simple_DnD_Builder.Models.Player;
+
+namespace Simple_DnD_Builder.Controllers
+{
+    public class PlayerController
+        : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            Player.PlayerPurger();
+            return (Player.Players.Count == 0) ? RedirectToAction("PlayerCreator") : RedirectToAction("PlayerChooser"); ;
+        }
+        public IActionResult ViewPlayer()
+        {
+            Player.PlayerPurger();
+            return View(Player.Players[0]) ;
+        }
+        public IActionResult PlayerChooser() 
+        {
+            Player.PlayerPurger();
+            return View(Player.Players);
+        }
+        public IActionResult PlayerCreator() 
+        {
+            Player.PlayerPurger();
+            return View(new Player());
+
+        }
+        public IActionResult CreatePlayer(Player player) 
+        { 
+            if (player.PlayerName == null || player.Name== null || player.Gender == null) 
+            {
+                return RedirectToAction("PlayerCreator");
+            }            
+            Player.Players.Add(player);
+            Player.PlayerPurger();
+            return RedirectToAction("ViewPlayer");
+        
+        }
+
+    }
+}
--- Models/Player/Player.cs
+++ Models/Player/Player.cs
@@ -6,14 +6,30 @@ namespace Simple_DnD_Builder.Models.Player
 {
     public class Player
     {
+        public static List<Player> Players = new List<Player>();
+        public Player()
+        {
+          
+        }
         public string Name { get; set; }
         public string PlayerName { get; set; }
+        public string Gender { get; set; }
         public Race Race { get; set; }
         public CharClass CharClass { get; set; }
-        public Dictionary<AbilityScore, int> AbilityScores { get; set; } 
+        public Dictionary<AbilityScore, int> AbilityScores { get; set; }
         public Alignment Alignment { get; set; }
         public List<Language> Languages { get; set; }
-        public Dictionary<Skill, int> SkillScores { get; set;}
+        public Dictionary<Skill, int> SkillScores { get; set; }
+        public string ImageURL { get; set; }
+
+        public static void PlayerPurger()
+        {
+           for(int i = Players.Count - 1; i > 0; i--)
+            {
+                Players.RemoveAt(i);
+            }
+            
+        }
 
     }
 }
